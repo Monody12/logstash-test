ac7152bd7b624fd66f74fcbf8c240462872af7bf
monody <qq614908309@gmail.com>
2024-12-09 22:05:39 +0800

    Initial commit

---
+++ src/main/java/org/example/logstashtest/MyController.java
@@ -0,0 +1,2 @@
+package org.example.logstashtest;public class MyController {
+}
---
+++ src/test/java/org/example/logstashtest/LogstashTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.logstashtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogstashTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
