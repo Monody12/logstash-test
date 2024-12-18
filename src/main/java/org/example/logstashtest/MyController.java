c36d085ce3cd4de68feff7ed80071f8f1ffd7220
Monody <qq614908309@gmail.com>
2024-12-18 21:59:26 +0800

    webhook test

--- src/main/java/org/example/logstashtest/MyController.java
+++ src/main/java/org/example/logstashtest/MyController.java
@@ -16,7 +16,7 @@ public class MyController {
     public String hello() {
         var now = LocalDateTime.now();
         logger.info("{} Hello", now);
-        return now + "Hello World  6666";
+        return now + "Hello World  66";
     }
 
 }
