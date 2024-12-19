2c4010eca6b488b6665a43ccccee010ab0245f34
monody <614908309@qq.com>
2024-12-19 22:00:04 +0800

    TEST

--- src/main/java/org/example/logstashtest/MyController.java
+++ src/main/java/org/example/logstashtest/MyController.java
@@ -16,7 +16,7 @@ public class MyController {
     public String hello() {
         var now = LocalDateTime.now();
         logger.info("{} Hello", now);
-        return now + "Hello World  99966 - TEST";
+        return now + "Hello World  99961 - TEST";
     }
 
 }
